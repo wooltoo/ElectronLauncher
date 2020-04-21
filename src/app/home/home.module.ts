52982b68abbbe5a033ea43bbb89a880fdf632bd7
Fredrik Sander <fredde.sander@gmail.com>
2020-04-21 20:22:53 +0200

    Added Bootstrap support

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,8 +6,10 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
+import { NavbarComponent } from '../navbar/navbar.component';
+
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, NavbarComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
