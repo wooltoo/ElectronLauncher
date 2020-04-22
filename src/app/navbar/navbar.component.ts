42abc23462b25a8ca948bde22a36295eec69c1fb
Fredrik Sander <fredde.sander@gmail.com>
2020-04-22 17:04:04 +0200

    Updated download bar design

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-const { shell } = require('electron')
+//const { shell } = require('electron')
 
 @Component({
   selector: 'app-navbar',
@@ -16,6 +16,6 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void { }
 
   openWebsite(suburl) {
-    shell.openExternal(this.website + suburl);
+    //shell.openExternal(this.website + suburl);
   }
 }
