5bed87eab2c0c14c8e2db39fc3c27646ae7699dd
Fredrik Sander <fredde.sander@gmail.com>
2020-04-24 09:43:38 +0200

    Early attempt at client download

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,33 @@ export class HomeComponent implements OnInit {
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    console.log("init");
+   }
 
+  download()
+  {
+
+    // https://www.npmjs.com/package/electron-download-manager
+
+    const { remote } = require('electron');
+    const asset = "https://dl.paragon-servers.com/Paragon_3.3.5a_Win.zip";
+    const target = "D:/DownloadTest";
+    remote.require("electron-download-manager").download({
+      url: asset,
+      downloadFolder: target,
+      onProgress: (progress, item) => {
+        console.log("progress: " + progress);
+      }
+    }, function(error, info) {
+      if (error) {
+        console.log("Error: " + error);
+      }
+      console.log(info);
+    });
+    //download(remote.BrowserWindow.getFocusedWindow(), asset, {
+    //  directory: "D:/DownloadTest"
+   // });
+    //https://dl.paragon-servers.com/Paragon_3.3.5a_Win.zip
+  }
 }
